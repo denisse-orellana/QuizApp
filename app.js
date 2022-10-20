9fb5ed05628c419b4103cc24c781c355e2e4f8e4
Denisse Orellana <denissep.orellana@gmail.com>
2022-10-20 17:29:12 -0300

    add: complete js $ styles

--- app.js
+++ app.js
@@ -1,15 +1,28 @@
-let input = document.querySelector('.initial__input')
+let welcomeScreen = document.getElementById('welcome')
+let resultsScreen = document.getElementById('results')
+let questions = document.querySelector('.quiz__questions')
+
+let input = document.querySelector('.welcome__input')
+let button = document.querySelector('.welcome__button')
 
 let questionAnswers = document.querySelector('.question__answers')
 
-let questions = document.querySelector('.quiz__questions')
-let end = document.querySelector('#quiz__final')
+let results = document.querySelector('#quiz__final')
 
 function Quiz() {
   this.questions = []
   this.counter = 0
+  this.indexCurrentQuestion = 0 
   this.addQuestion = function(question) {
     this.questions.push(question)
+  }
+  this.showCurrentQuestion = function() {
+    if (this.indexCurrentQuestion < this.questions.length) {
+      this.questions[this.indexCurrentQuestion].getElement()
+    } else {
+      questions.style.display = 'none'
+      resultsScreen.style.display = 'block'
+    }
   }
 }
 
@@ -27,33 +40,70 @@ function Question(title, answers) {
     return question
   }
   this.getElement = function() {
-    let name = document.createElement('h2')
-    name.textContent = input.value
+    let name = document.createElement('h1')
+    name.textContent = `Welcome @${input.value.toLowerCase()}`
     questions.append(name)
 
     let questionTitle = document.createElement('h3')
     questionTitle.textContent = this.title
     questions.append(questionTitle)
 
+    let questionAnswers = document.createElement("ul")
+    questionAnswers.className = "question__awswers"
+
     this.answers.forEach((answer, index) => {
-      let answers = document.createElement('li')
-      answers.textContent = answer
-      answers.id = index+1
-      questionAnswers.append(answers)
+      let theanswer = document.createElement('li')
+      theanswer.textContent = answer
+      theanswer.id = index+1
+      theanswer.addEventListener("click", this.saveAnswer)
+      questionAnswers.append(theanswer)
     })
 
     questions.append(questionAnswers)
   }
+  this.saveAnswer = (event) => {
+    let allAswers = []
+    let selectAnswer = event.target
+    allAswers.push(selectAnswer.id)
+    quiz.counter++
+
+    setTimeout(function(){
+      questions.textContent = ''
+      quiz.indexCurrentQuestion++
+      quiz.showCurrentQuestion()
+    }, 1000)
+
+    console.log(allAswers)
+  }
 }
-
-let q1 = new Question('Question 1', ['Answer 1', 'Answer 2', 'Answer 3'])
-
-let quiz = new Quiz()
-quiz.addQuestion(q1)
 
 function startQuiz() {
-  let start = document.getElementById('start')
-  start.style.display = 'none'
-
-  // questions.style.display = 'block'
+  let welcome = document.getElementById('welcome')
+  welcome.style.display = 'none'
+  questions.style.display = 'block'
+  quiz.showCurrentQuestion()
 }
+
+
+let question1 = new Question('Did you make the last purchase online on our website Migros?', ['Yes', 'No'])
+let question2 = new Question('Lorem ipsum dolor sit amet consectetur, adipiscing elit sapien lobortis', ['Luctus nullam', 'Convallis auctor penatibus', 'Orci a lobortis vivamus porttitor dapibus'])
+let question3 = new Question('Dolor sit amet consectetur adipiscing elit quam', ['Luctus nullam', 'Convallis auctor penatibus', 'Orci a lobortis vivamus porttitor dapibus'])
+let question4 = new Question('Integer quis natoque nunc dignissim nisl dui accumsan platea', ['Luctus nullam', 'Convallis auctor penatibus', 'Orci a lobortis vivamus porttitor dapibus'])
+let question5 = new Question('Hendrerit dis potenti inceptos turpis egestas dictum', ['Luctus nullam', 'Convallis auctor penatibus', 'Orci a lobortis vivamus porttitor dapibus'])
+let question6 = new Question('Pretium laoreet imperdiet porttitor odio elementum donec sollicitudin', ['Luctus nullam', 'Convallis auctor penatibus', 'Orci a lobortis vivamus porttitor dapibus'])
+let question7 = new Question('Dui fermentum cursus consequat pharetra class tempor', ['Luctus nullam', 'Convallis auctor penatibus', 'Orci a lobortis vivamus porttitor dapibus'])
+let question8 = new Question('Pulvinar ut sociis diam enim class litora', ['Luctus nullam', 'Convallis auctor penatibus', 'Orci a lobortis vivamus porttitor dapibus'])
+let question9 = new Question('Eget porta sodales ligula venenatis fermentum nunc', ['Luctus nullam', 'Convallis auctor penatibus', 'Orci a lobortis vivamus porttitor dapibus'])
+let question10 = new Question('Auctor accumsan hac turpis tempus', ['Luctus nullam', 'Convallis auctor penatibus', 'Orci a lobortis vivamus porttitor dapibus'])
+
+let quiz = new Quiz()
+quiz.addQuestion(question1)
+quiz.addQuestion(question2)
+quiz.addQuestion(question3)
+quiz.addQuestion(question4)
+quiz.addQuestion(question5)
+quiz.addQuestion(question6)
+quiz.addQuestion(question7)
+quiz.addQuestion(question8)
+quiz.addQuestion(question9)
+quiz.addQuestion(question10)
